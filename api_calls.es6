26fa84fa3d90f0f4148ec5a256ce02e639fef6f2
Adam Recvlohe <adam_recvlohe@valpak.com>
2015-10-23 14:05:22 -0400

    refactor with promise.all

--- api_calls.es6
+++ api_calls.es6
@@ -6,73 +6,44 @@ let cities = [
   'New York, NY'
 ];
 
-
-function getUrls() {
+function getWeatherDataFrom(city) {
   return new Promise((resolve) => {
-    resolve(
-      cities.map((city) => {
-       return ['http://api.openweathermap.org/data/2.5/weather?q=',city,'&appid=bd82977b86bf27fb59a04b61b657fb6f'
-       ].join('');
-      })
-    );
-  });
-}
-
-
-function getWeatherData(urls) {
-  var count = 0;
-  var feed = [];
-  return new Promise((resolve) => {
-    feed = urls.map((url, idx) => {
-      http.get(url, (res) => {
-        feed[idx] = '';
-        res.on('error', error => {
-          console.error(error);
-        });
-        res.on('data', chunk => {
-          feed[idx]+=chunk;
-        });
-        res.on('end', () => {
-          count+=1;
-          console.log(`logged entry ${idx}`);
-          if(count==cities.length) {
-            resolve(feed);
-          }
-        });
+    var body = '';
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd82977b86bf27fb59a04b61b657fb6f`;
+    http.get(url, (res) => {
+      res.on('error', error => {
+        console.error(error);
+      });
+      res.on('data', chunk => {
+        body+=chunk;
+      });
+      res.on('end', () => {
+        resolve(JSON.parse(body).main.temp);
       });
     });
   });
 }
 
 
-function parseData(allData) {
-  var tempAvg;
-  return new Promise((resolve) => {
-    resolve(
-      tempAvg = allData.map((data) => {
-        return JSON.parse(data).main.temp;
-      }).reduce((prv, nxt) => {
-        var sum = prv + nxt;
-        var avg = sum / cities.length + 1;
-        return avg;
-      })
-    );
+function parseDataFrom(weatherData) {
+  var sum = weatherData.reduce((prv, nxt) => {
+    return prv + nxt;
+  }, 0);
+  return sum / weatherData.length;
+}
+
+function handleResponse(req, res) {
+  var promises = cities.map((city) => {
+    return getWeatherDataFrom(city);
   });
+  Promise.all(promises)
+    .then(parseDataFrom)
+    .then(temp => {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(`The average temperature for [${cities}] is ${temp.toFixed(1)} degrees Kelvin`);
+      res.end(console.log('Retrieving info'));
+    })
+    .catch(error => console.error(error));
 }
 
-
-function serve(temp) {
-  http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(`The average temperature for ${cities[0]}, ${cities[1]}, ${cities[2]}, and ${cities[3]} is ${temp.toFixed(1)} degrees Kelvin`);
-    res.end(console.log('Retrieving info'));
-  }).listen(3000);
-
-}
-
-
-getUrls()
-  .then(results => getWeatherData(results)
-  .then(results => parseData(results)
-  .then(result => serve(result))
-  .catch(error => console.error(error))));
+http.createServer(handleResponse).listen(3000);
--- api_calls.js
+++ api_calls.js
@@ -3,66 +3,42 @@
 var http = require('http');
 var cities = ['Miami, FL', 'Tampa, FL', 'Seattle, WA', 'New York, NY'];
 
-function getUrls() {
+function getWeatherDataFrom(city) {
   return new Promise(function (resolve) {
-    resolve(cities.map(function (city) {
-      return ['http://api.openweathermap.org/data/2.5/weather?q=', city, '&appid=bd82977b86bf27fb59a04b61b657fb6f'].join('');
-    }));
-  });
-}
-
-function getWeatherData(urls) {
-  var count = 0;
-  var feed = [];
-  return new Promise(function (resolve) {
-    feed = urls.map(function (url, idx) {
-      http.get(url, function (res) {
-        feed[idx] = '';
-        res.on('error', function (error) {
-          console.error(error);
-        });
-        res.on('data', function (chunk) {
-          feed[idx] += chunk;
-        });
-        res.on('end', function () {
-          count += 1;
-          console.log('logged entry ' + idx);
-          if (count == cities.length) {
-            resolve(feed);
-          }
-        });
+    var body = '';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=bd82977b86bf27fb59a04b61b657fb6f';
+    http.get(url, function (res) {
+      res.on('error', function (error) {
+        console.error(error);
+      });
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
+      res.on('end', function () {
+        resolve(JSON.parse(body).main.temp);
       });
     });
   });
 }
 
-function parseData(allData) {
-  var tempAvg;
-  return new Promise(function (resolve) {
-    resolve(tempAvg = allData.map(function (data) {
-      return JSON.parse(data).main.temp;
-    }).reduce(function (prv, nxt) {
-      var sum = prv + nxt;
-      var avg = sum / cities.length + 1;
-      return avg;
-    }));
-  });
+function parseDataFrom(weatherData) {
+  var sum = weatherData.reduce(function (prv, nxt) {
+    return prv + nxt;
+  }, 0);
+  return sum / weatherData.length;
 }
 
-function serve(temp) {
-  http.createServer(function (req, res) {
+function handleResponse(req, res) {
+  var promises = cities.map(function (city) {
+    return getWeatherDataFrom(city);
+  });
+  Promise.all(promises).then(parseDataFrom).then(function (temp) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('The average temperature for ' + cities[0] + ', ' + cities[1] + ', ' + cities[2] + ', and ' + cities[3] + ' is ' + temp.toFixed(1) + ' degrees Kelvin');
+    res.write('The average temperature for [' + cities + '] is ' + temp.toFixed(1) + ' degrees Kelvin');
     res.end(console.log('Retrieving info'));
-  }).listen(3000);
+  })['catch'](function (error) {
+    return console.error(error);
+  });
 }
 
-getUrls().then(function (results) {
-  return getWeatherData(results).then(function (results) {
-    return parseData(results).then(function (result) {
-      return serve(result);
-    })['catch'](function (error) {
-      return console.error(error);
-    });
-  });
-});
+http.createServer(handleResponse).listen(3000);
