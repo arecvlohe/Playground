2795c5ba437fd06811d68a393f7c350b1a9ae398
Adam Recvlohe <adam_recvlohe@valpak.com>
2015-10-30 14:38:11 -0400

    change tabs to spaces

--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -4,53 +4,53 @@ var mongoose = require('mongoose');
 var Superhero = mongoose.model('superheroes');
 
 router.get('/superheroes', function (req, res) {
-	Superhero.find(function (err, superheroes){
-		console.log(superheroes);	
-		res.render(
-			'api', 
-			{title: 'Superhero API', superheroes : superheroes}
-		);
-	});
+  Superhero.find(function (err, superheroes){
+    console.log(superheroes); 
+    res.render(
+      'api', 
+      {title: 'Superhero API', superheroes : superheroes}
+    );
+  });
 });
 
 router.get('/superhero/:id', function (req, res) {
-	var query = {"_id": req.params.id};
-	Superhero.findOne(query, function(err, superhero) {
-		console.log(superhero);
-		res.render(
-			'superhero',
-			{title: 'Superhero API - '+ superhero.name, superhero : superhero}	
-		);
-	});
+  var query = {"_id": req.params.id};
+  Superhero.findOne(query, function(err, superhero) {
+    console.log(superhero);
+    res.render(
+      'superhero',
+      {title: 'Superhero API - '+ superhero.name, superhero : superhero}  
+    );
+  });
 });
 
 router.post('/superheroes', function(req, res) {
-	new Superhero({name: req.body.name})
-	.save(function(err, superhero){ 
-		console.log(superhero);
-		res.redirect('/api/superheroes');	
-	});
+  new Superhero({name: req.body.name})
+  .save(function(err, superhero){ 
+    console.log(superhero);
+    res.redirect('/api/superheroes'); 
+  });
 });
 
 router.put('/superhero/:id', function(req, res) {
-	var query = {"_id": req.params.id};
-	var update = {name: req.body.name};
-	var options = {new: true};
-	Superhero.findOneAndUpdate(query, update, options, function(err, superhero) {
-		console.log(superhero);
-		res.render(
-			'superhero',
-			{title: 'Superhero API - ' + superhero.name, superhero : superhero}	
-		);	
-	});
+  var query = {"_id": req.params.id};
+  var update = {name: req.body.name};
+  var options = {new: true};
+  Superhero.findOneAndUpdate(query, update, options, function(err, superhero) {
+    console.log(superhero);
+    res.render(
+      'superhero',
+      {title: 'Superhero API - ' + superhero.name, superhero : superhero} 
+    );  
+  });
 });
 
 router.delete('/superhero/:id', function(req, res) {
-	var query = {"_id": req.params.id};
-	Superhero.findOneAndRemove(query, function(err, superhero) {
-		console.log(superhero);
-		res.redirect('/api/superheroes');	
-	});
+  var query = {"_id": req.params.id};
+  Superhero.findOneAndRemove(query, function(err, superhero) {
+    console.log(superhero);
+    res.redirect('/api/superheroes'); 
+  });
 });
 
 module.exports = router;
